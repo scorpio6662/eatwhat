48e8ff8373122660a9a01acbe23f2d8c3e498c6e
wangyue132 <wangyue132@meituan.com>
2021-03-24 11:17:47 +0800

    init

---
+++ homepage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>title</h1>
+  </div>
+  <div>
+    <el-form>
+      <h2>这里预计一个list</h2>
+    </el-form>
+    <el-table>
+      <el-table-colum label="序号"></el-table-colum>
+      <el-table-colum label="地点"></el-table-colum>
+      <el-table-colum label="餐厅名称"></el-table-colum>
+      <el-table-colum label="菜系"></el-table-colum>
+      <el-table-colum label="必吃菜品"></el-table-colum>
+      <el-table-colum label="备注"></el-table-colum>
+    </el-table>
+  </div>
+</template>
